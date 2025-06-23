9ffdce190d436b022debb1fc7e79ae7b2b69967d
“1810551251” <1810551251@qq.com>
2025-06-23 21:11:08 +0800

    fix:添加图片域名

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.hitpaw.*',
+      },
+      
+    ],
+    // 可选：添加其他图像优化配置
+    // formats: ['image/webp'],
+    // minimumCacheTTL: 60,
+  }
 };
 
 export default nextConfig;
